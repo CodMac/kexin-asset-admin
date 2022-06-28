e9189458c843a98a288dd3310401eeb1962a5618
CodMac <19941205Mc>
2022-06-28 11:50:03 +0800

    添加lombok

--- kexin-asset-common/src/main/java/com/kexin/common/core/page/PageDomain.java
+++ kexin-asset-common/src/main/java/com/kexin/common/core/page/PageDomain.java
@@ -1,12 +1,14 @@
 package com.kexin.common.core.page;
 
 import com.kexin.common.utils.StringUtils;
+import lombok.Data;
 
 /**
  * 分页数据
  *
  * @author ruoyi
  */
+@Data
 public class PageDomain {
     /**
      * 当前记录起始索引
@@ -40,34 +42,6 @@ public class PageDomain {
         return StringUtils.toUnderScoreCase(orderByColumn) + " " + isAsc;
     }
 
-    public Integer getPageNum() {
-        return pageNum;
-    }
-
-    public void setPageNum(Integer pageNum) {
-        this.pageNum = pageNum;
-    }
-
-    public Integer getPageSize() {
-        return pageSize;
-    }
-
-    public void setPageSize(Integer pageSize) {
-        this.pageSize = pageSize;
-    }
-
-    public String getOrderByColumn() {
-        return orderByColumn;
-    }
-
-    public void setOrderByColumn(String orderByColumn) {
-        this.orderByColumn = orderByColumn;
-    }
-
-    public String getIsAsc() {
-        return isAsc;
-    }
-
     public void setIsAsc(String isAsc) {
         if (StringUtils.isNotEmpty(isAsc)) {
             // 兼容前端排序类型
@@ -87,7 +61,4 @@ public class PageDomain {
         return reasonable;
     }
 
-    public void setReasonable(Boolean reasonable) {
-        this.reasonable = reasonable;
-    }
 }
